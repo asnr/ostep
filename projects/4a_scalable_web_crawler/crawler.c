8ccea5f3e658f1e8d836f6c8c632d4b14f8028a1
asnr <asnr@users.noreply.github.com>
2018-08-18 15:59:19 +1000

    Find all links in a page

--- projects/4a_scalable_web_crawler/crawler.c
+++ projects/4a_scalable_web_crawler/crawler.c
@@ -11,7 +11,7 @@
 const char LINK_PREFIX[] = "link:";
 
 const size_t MAX_URL_SIZE = 1028;
-char url_buffer[MAX_URL_SIZE];
+char url_buffer[MAX_URL_SIZE + 1];
 
 void parse_page(char *url, char *contents, void (*_edge_fn)(char *from, char *to));
 
@@ -30,32 +30,49 @@ int crawl(char *start_url,
   return -1;
 }
 
-bool copy_url_to_buffer(char *link_prefix, char *buffer, size_t buffer_size)
+bool is_url_terminator(char c)
+{
+  return isspace(c) || c == '\0';
+}
+
+size_t url_length(char *url)
+{
+  size_t length = 0;
+  while (!is_url_terminator(*(url+length))) {
+    length++;
+  }
+  return length;
+}
+
+char *copy_url_to_buffer(char *link_prefix, char *buffer, size_t buffer_size)
 {
   size_t prefix_length = sizeof(LINK_PREFIX) - 1;
   char *url_start = link_prefix + prefix_length;
-  if (isspace(*url_start)) { return false; }
 
-  for (size_t i = 0; i < buffer_size - 1; i++) {
-    char curr_char = *(url_start + i);
-    if (isspace(curr_char)) {
-      *(buffer + i) = '\0';
-      return true;
-    }
-
-    *(buffer + i) = curr_char;
+  size_t length = url_length(url_start);
+  if (0 < length && length < buffer_size) {
+    memcpy(buffer, url_start, length);
+    *(buffer + length) = '\0';
+  } else {
+    *buffer = '\0';
   }
 
-  return false;
+  return url_start + length;
 }
 
 void parse_page(char *url, char *contents, void (*_edge_fn)(char *from, char *to))
 {
-  char *link_prefix = strstr(contents, LINK_PREFIX);
-  if (copy_url_to_buffer(link_prefix, url_buffer, MAX_URL_SIZE)) {
-    /* printf(">>>>>>>>> found URL: %s", url_buffer); */
-    _edge_fn(url, url_buffer);
-  } else {
-    // link too long or empty, do something?
+  char *position = contents;
+  char *next_link_prefix = strstr(position, LINK_PREFIX);
+  while (next_link_prefix != NULL) {
+    position = copy_url_to_buffer(next_link_prefix, url_buffer, MAX_URL_SIZE);
+    bool valid_url = *url_buffer != '\0';
+    if (valid_url) {
+      _edge_fn(url, url_buffer);
+    } else {
+      // link too long or empty, do something?
+    }
+
+    next_link_prefix = strstr(position, LINK_PREFIX);
   }
 }
